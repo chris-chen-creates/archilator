7670039fffd438a4e0c1296a2bdcb1ea23f5fba0
angles-n-daemons <dillmann.brian@gmail.com>
2021-03-03 10:34:05 -0500

    add tsconfig

---
+++ src/Calculator.ts
@@ -0,0 +1,46 @@
+import { createInterface } from 'readline';
+
+class Calculator {
+	private rl = createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	})
+
+	async run() {
+		this.recursivePrompt();
+	}
+
+	async recursivePrompt() {
+		this.rl.question("> ", (line: string) => {
+			console.log(line);
+			this.recursivePrompt();
+		});
+	}
+}
+
+function sleep(ms: number) {
+	return new Promise((resolve) => {
+		setTimeout(resolve, ms);
+	});
+}
+
+async function startupPrompt() {
+	console.warn("archilator initiating...");
+	await sleep(1000);
+	console.log("stand by.");
+	await sleep(1000);
+	console.log(".");
+	await sleep(100);
+	console.log("..");
+	await sleep(50);
+	console.log("... ->");
+	console.log("system stack loaded.");
+    console.log("begin input sequence.");
+}
+
+async function main() {
+	await startupPrompt();
+	await (new Calculator()).run();
+}
+
+main();
