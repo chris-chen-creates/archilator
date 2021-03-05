257c7877629a0ab9361ec599693064a92057855a
Brian Dillmann <dillmann.brian@gmail.com>
2021-03-05 11:28:06 -0500

    fix cadams branch

--- src/Scanner.ts
+++ src/Scanner.ts
@@ -1,52 +1,175 @@
-import { Token, TokenType } from './Token';
+import { Token, TokenType } from './Token'
 
-class ScannerError extends Error {};
+class ScannerError extends Error {}
 
 function scan(input: string): Token[] {
-	return new Scanner(input).scanTokens();
+  return new Scanner(input).scanTokens()
 }
 
 export class Scanner {
-	public index: number = 0;
+  public index: number = 0
 
-	constructor(
-		public input: string,
-	) {}
+  constructor(public input: string) {}
 
-	scanTokens(): Token[] {
-		const tokens = [];
+  scanTokens(): Token[] {
+    const tokens = []
 
-		while (!this.isAtEnd()) {
-			const token = this.scanNext();
-			if (token) {
-				tokens.push(token);
-			}
-		}
+    while (!this.isAtEnd()) {
+      const token = this.scanNext()
+      if (token) {
+        tokens.push(token)
+      }
+    }
 
-		return tokens;
-	}
+    return tokens
+  }
 
-	scanNext(): Token {
-		const char = this.advance();
+  scanNext(): Token {
+    const char = this.advance()
 
-		if (char === "+") {
-			return {
-				ttype: TokenType.PLUS,
-				text: "+",
-			};
-		}
-		throw(new ScannerError(`scanNext: unrecognized character ${char}`));
-	}
+    switch (char) {
+      case '+':
+        return {
+          ttype: TokenType.PLUS,
+          text: '+',
+        }
+        break
+      case '-':
+        return {
+          ttype: TokenType.MINUS,
+          text: '-',
+        }
+        break
+      case '*':
+        return {
+          ttype: TokenType.TIMES,
+          text: '*',
+        }
+        break
+      case '/':
+        return {
+          ttype: TokenType.DIVIDE,
+          text: '/',
+        }
+        break
+      case '(':
+        return {
+          ttype: TokenType.LEFT_PAREN,
+          text: '(',
+        }
+        break
+      case ')':
+        return {
+          ttype: TokenType.RIGHT_PAREN,
+          text: ')',
+        }
+        break
+      case '^':
+        return {
+          ttype: TokenType.POW,
+          text: '^',
+        }
+        break
+      case '0':
+        return {
+          ttype: TokenType.NUMBER,
+          text: '0',
+        }
+        break
+      case '1':
+        return {
+          ttype: TokenType.NUMBER,
+          text: '1',
+        }
+        break
+      case '2':
+        return {
+          ttype: TokenType.NUMBER,
+          text: '2',
+        }
+        break
+      case '3':
+        return {
+          ttype: TokenType.NUMBER,
+          text: '3',
+        }
+        break
+      case '4':
+        return {
+          ttype: TokenType.NUMBER,
+          text: '4',
+        }
+        break
+      case '5':
+        return {
+          ttype: TokenType.NUMBER,
+          text: '5',
+        }
+        break
+      case '6':
+        return {
+          ttype: TokenType.NUMBER,
+          text: '6',
+        }
+        break
+      case '7':
+        return {
+          ttype: TokenType.NUMBER,
+          text: '7',
+        }
+        break
+      case '8':
+        return {
+          ttype: TokenType.NUMBER,
+          text: '8',
+        }
+        break
+      case '9':
+        return {
+          ttype: TokenType.NUMBER,
+          text: '9',
+        }
+        break
+      // Trying to get pass scanTokens ignores spaces
+      // case '':
+      // return {
+      //   ttype:
+      //   text: '',
+      // }
+      //   break
 
-	isAtEnd(): boolean {
-		throw(new ScannerError("isAtEnd: not implemented"));
-	}
+      // Not sure why the default is not throwing an error properly
+      default:
+        throw new ScannerError(`scanNext: unexpected character: ${char}`)
+        break
+    }
+  }
 
-	peek(): string {
-		throw(new ScannerError("peek: not implemented"));
-	}
+  match(): boolean {
+    if (this.isAtEnd()) return false
+    if (this.input.charAt(this.index)/* Find the equivalent of != expected */) return false
+    //unsure about this ^^^
 
-	advance(): string {
-		throw(new ScannerError("advance: not implemented"));
-	}
+    this.index++
+    return true
+  }
+
+  isAtEnd(): boolean {
+    if (this.index < 0) {
+      throw new ScannerError(`index is negative and cannot be negative`)
+    }
+    return this.index >= this.input.length
+  }
+
+  peek(): string {
+    throw new ScannerError('peek: not implemented')
+  }
+  // Figure out what's wrong with this throwing an error when it moves past the end.
+  advance(): string {
+    // if (this.index > this.input.length) {
+    return this.input.charAt(this.index++)
+    // } else {
+    //   throw new ScannerError('Input is at end')
+    // }
+  }
 }
--- src/__tests__/Scanner.test.ts
+++ src/__tests__/Scanner.test.ts
@@ -1,103 +1,116 @@
-import { Scanner } from '../Scanner';
-import { TokenType } from '../Token';
+import { Scanner } from '../Scanner'
+import { TokenType } from '../Token'
 
-const each = require("jest-each").default
+/*
+isAtEnd basic test -- passed
+isAtEnd negative index -- passed
+advance basic test -- passed
+advance moves index -- passed
+advance throws error past end
+scanNext simple characters -- passed
+scanNext properly handles numbers
+scanNext throws an error if it does not recognize the character
+scanTokens ignores spaces
+*/
 
-test.each([
-	["", 0, true],
-	["a", 0, false],
-	["a", 1, true],
-	["a", 2, true],
-	["a bit longer input", 0, false],
-	["a bit longer input", 5, false],
-	["a bit longer input", 17, false],
-	["a bit longer input", 18, true],
-	["a bit longer input", 19, true],
-	["a bit longer input", 50, true],
-])(
-	"isAtEnd basic test",
-	(input: string, index: number, expected: boolean) => {
-		const scanner = new Scanner(input);
-		scanner.index = index;
-		expect(scanner.isAtEnd()).toEqual(expected);
-	}
-);
-
-test("isAtEnd negative index", () => {
-	const scanner = new Scanner("");
-	scanner.index = -1;
-	// Error text should contain: 'negative"
-	expect(scanner.isAtEnd()).toThrow(/.*negative.*/);
-});
+const each = require('jest-each').default
 
 test.each([
-	["abcde", 0, "a"],
-	["abcde", 1, "b"],
-	["abcde", 4, "e"],
-	["ab de", 2, " "],
-])(
-	"advance basic test",
-	(input, index, expected) => {
-		const scanner = new Scanner(input);
-		scanner.index = index;
-		expect(scanner.advance()).toEqual(expected);
-	},
-);
+  ['', 0, true],
+  ['a', 0, false],
+  ['a', 1, true],
+  ['a', 2, true],
+  ['a bit longer input', 0, false],
+  ['a bit longer input', 5, false],
+  ['a bit longer input', 17, false],
+  ['a bit longer input', 18, true],
+  ['a bit longer input', 19, true],
+  ['a bit longer input', 50, true],
+])('isAtEnd basic test', (input: string, index: number, expected: boolean) => {
+  const scanner = new Scanner(input)
+  scanner.index = index
+  expect(scanner.isAtEnd()).toEqual(expected)
+})
 
-test("advance moves index", () => {
-	const scanner = new Scanner("abcde");
-	expect(scanner.index).toEqual(0);
-	scanner.advance();
-	expect(scanner.index).toEqual(1);
-});
-
-test("advance throws error past end", () => {
-	const scanner = new Scanner("abcde");
-	scanner.index = 4;
-	// Error text should contain: 'negative"
-	expect(scanner.advance()).toThrow(/.*at end.*/);
-});
+test('isAtEnd negative index', () => {
+  const scanner = new Scanner('')
+  scanner.index = -1
+  // Error text should contain: 'negative"
+  expect(() => {
+    scanner.isAtEnd()
+  }).toThrow(/.*negative.*/)
+})
 
 test.each([
-	["+", TokenType.PLUS],
-	["-", TokenType.MINUS],
-	["*", TokenType.TIMES],
-	["/", TokenType.DIVIDE],
-	["(", TokenType.LEFT_PAREN],
-	[")", TokenType.RIGHT_PAREN],
-	["^", TokenType.POW],
-	["0", TokenType.NUMBER],
-	["1", TokenType.NUMBER],
-	["2", TokenType.NUMBER],
-	["3", TokenType.NUMBER],
-	["4", TokenType.NUMBER],
-	["5", TokenType.NUMBER],
-	["6", TokenType.NUMBER],
-	["7", TokenType.NUMBER],
-	["8", TokenType.NUMBER],
-	["9", TokenType.NUMBER],
-])("scanNext simple characters", (input: string, ttype: TokenType) => {
-	const token = new Scanner(input).scanNext();
-	expect(token.ttype).toEqual(ttype);
-});
+  ['abcde', 0, 'a'],
+  ['abcde', 1, 'b'],
+  ['abcde', 4, 'e'],
+  ['ab de', 2, ' '],
+])('advance basic test', (input, index, expected) => {
+  const scanner = new Scanner(input)
+  scanner.index = index
+  expect(scanner.advance()).toEqual(expected)
+})
 
-test("scanNext properly handles numbers", () => {
-	let token = new Scanner("0").scanNext();
-	expect(token.literal).toEqual(0);
-	token = new Scanner("9").scanNext();
-	expect(token.literal).toEqual(9);
-});
+test('advance moves index', () => {
+  const scanner = new Scanner('abcde')
+  expect(scanner.index).toEqual(0)
+  scanner.advance()
+  expect(scanner.index).toEqual(1)
+})
 
-test("scanNext throws an error if it does not recognize the character", () => {
-	const scanner = new Scanner(">")
-	// Error text should contain: 'unexpected character"
-	expect(scanner.scanNext).toThrow(/.*unexpected character.*/);
-});
+test('advance throws error past end', () => {
+  const scanner = new Scanner('abcde')
+  scanner.index = 5
+  // Error text should contain: 'negative"
+  expect(() => {
+    scanner.advance()
+  }).toThrow(/.*at end.*/)
+  //expect(scanner.advance()).toThrow(/.*at end.*/)
+})
 
-test("scanTokens ignores spaces", () => {
-	const tokens = new Scanner(" +").scanTokens();
-	expect(tokens).toEqual([{
-		ttype: TokenType.PLUS,
-		text: "+",
-	}]);
-});
+test.each([
+  ['+', TokenType.PLUS],
+  ['-', TokenType.MINUS],
+  ['*', TokenType.TIMES],
+  ['/', TokenType.DIVIDE],
+  ['(', TokenType.LEFT_PAREN],
+  [')', TokenType.RIGHT_PAREN],
+  ['^', TokenType.POW],
+  ['0', TokenType.NUMBER],
+  ['1', TokenType.NUMBER],
+  ['2', TokenType.NUMBER],
+  ['3', TokenType.NUMBER],
+  ['4', TokenType.NUMBER],
+  ['5', TokenType.NUMBER],
+  ['6', TokenType.NUMBER],
+  ['7', TokenType.NUMBER],
+  ['8', TokenType.NUMBER],
+  ['9', TokenType.NUMBER],
+])('scanNext simple characters', (input: string, ttype: TokenType) => {
+  const token = new Scanner(input).scanNext()
+  expect(token.ttype).toEqual(ttype)
+})
+
+test('scanNext properly handles numbers', () => {
+  let token = new Scanner('0').scanNext()
+  expect(token.literal).toEqual(0)
+  token = new Scanner('9').scanNext()
+  expect(token.literal).toEqual(9)
+})
+
+test('scanNext throws an error if it does not recognize the character', () => {
+  const scanner = new Scanner('>')
+  // Error text should contain: 'unexpected character"
+  expect(scanner.scanNext).toThrow(/.*unexpected character.*/)
+})
+
+test('scanTokens ignores spaces', () => {
+  const tokens = new Scanner(' +').scanTokens()
+  expect(tokens).toEqual([
+    {
+      ttype: TokenType.PLUS,
+      text: '+',
+    },
+  ])
+})
