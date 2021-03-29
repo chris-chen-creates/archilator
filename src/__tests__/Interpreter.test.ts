b0af0eeeab3843951807ff2be2ca42cab8f621dd
Christian Adams <cadams@squareup.com>
2021-03-29 16:58:10 -0500

    added a test file for Interpreter

---
+++ src/__tests__/Interpreter.test.ts
@@ -0,0 +1,14 @@
+import {
+  Addition,
+  Multiplication,
+  Negative,
+  Group,
+  Num,
+  Division,
+} from '../Expression'
+import { Interpreter } from '../Interpreter'
+import { scan } from '../Scanner'
+
+// test('addition works correctly', () => {
+//   const eval = new Interpreter(scan('5 + 5'))
+// })
